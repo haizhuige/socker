9e5446ab9e74968844c5b9ffc877f5168530e583
j-liuhu-jk <liuhu-jk@360jinrong.net>
2019-08-04 10:04:39 +0800

    添加注释

--- src/main/java/com/liuhu/socket/service/impl/SharesInfoServiceImpl.java
+++ src/main/java/com/liuhu/socket/service/impl/SharesInfoServiceImpl.java
@@ -1,19 +1,17 @@
 package com.liuhu.socket.service.impl;
 
-import com.liuhu.socket.common.DateUtils;
-import com.liuhu.socket.common.MathConstants;
 import com.liuhu.socket.dao.MarketInfoMapper;
 import com.liuhu.socket.dao.ShareInfoMapper;
 import com.liuhu.socket.domain.MarketInputDomain;
 import com.liuhu.socket.domain.MarketOutputDomain;
 import com.liuhu.socket.entity.MarketInfo;
-import com.liuhu.socket.entity.ShareInfo;
 import com.liuhu.socket.enums.SpecialSockerEnum;
 import com.liuhu.socket.service.SharesInfoService;
-import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -21,8 +19,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.StringUtils;
 
 @Service
 public class SharesInfoServiceImpl implements SharesInfoService {
@@ -41,19 +37,27 @@ public class SharesInfoServiceImpl implements SharesInfoService {
 
     @Override
     public List<MarketOutputDomain> getRiseOfRate(MarketInputDomain input) {
-        List<MarketOutputDomain> outList = new ArrayList<MarketOutputDomain>();
+        List<MarketOutputDomain> outList = new ArrayList<>();
+        /**
+         * 获取一段时间内的收益率
+         */
         List<MarketOutputDomain> list = marketInfoMapper.getLastEndList(input);
         MarketInputDomain aMarket = new MarketInputDomain();
         BeanUtils.copyProperties(input, aMarket);
         aMarket.setShareCode(SpecialSockerEnum.A_SOCKER.getCode());
-        MarketOutputDomain aOutPut = new MarketOutputDomain();
+        MarketOutputDomain aOutPut;
+        /**
+         * 获取上证指数收益率
+         */
         if (input != null && StringUtils.isNotEmpty(input.getShareCode())) {
             aOutPut = marketInfoMapper.getLastEndList(aMarket).get(0);
         } else {
             aOutPut = list.stream().filter(marketOutputDomain -> SpecialSockerEnum.A_SOCKER.getCode().equals(marketOutputDomain.getShareCode())).findFirst().get();
         }
+        /**
+         * 去掉list中上证指数数据
+         */
         for (MarketOutputDomain outPut : list) {
-
             outPut.setARate(aOutPut.getRate());
             if (!SpecialSockerEnum.A_SOCKER.getCode().equals(outPut.getShareCode())) {
                 outList.add(outPut);
